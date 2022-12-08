19e1676af64ee1173ddde4dacc041b602574cf94
dyxzkh <dyxzkh@gmail.com>
2022-12-08 15:37:37 +0700

    music-bot

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const client = new Discord.Client({
 	intents: ['MessageContent','Guilds', 'GuildVoiceStates', 'GuildMessages'],
 });
 
+let channel;
+let author;
 // Create a new DisTube
 const distube = new DisTube(client, {
 	emitNewSongOnly: false,
@@ -17,9 +19,10 @@ const distube = new DisTube(client, {
 client.on('ready', client => {
   	console.log(`KSK is Online!`);
   	client.user.setStatus("online");
-  	distube.on('error', (channel, error) => {
+	client.user.setActivity(`Your Feeling`,Discord.ActivityType.Playing);
+  	distube.on('error', (error) => {
     console.error(error)
-    channel.send(`An error encoutered: ${error.slice(0, 1979)}`) // Discord limits 2000 characters in a message
+    //channel.send(`An error encoutered: ${error.slice(0, 1979)}`) // Discord limits 2000 characters in a message
   })
 });
 // client.on("debug", console.log)
@@ -33,20 +36,116 @@ client.on('messageCreate', message => {
 		.split(/ +/g);
 	const cmd = args.shift();
 
-	if(cmd === 'ping'){
+	channel = message.channel;
+	author = message.author;
+
+	if(cmd === 'ching'){
 		message.channel.send("Ching Chong!");
 	}
 
 	if (cmd === 'play' || cmd === 'p') {
 		const voiceChannel = message.member?.voice?.channel;
 		if (voiceChannel) {
-			distube.play(voiceChannel, args.join(' '));
+			if(args.join(' ') === ""){
+				message.channel.send(
+					'>>> សូមបញ្ជូលឈ្មោះចម្រៀងឬលីងក្នុង cmd!😑',
+				);
+				return;
+			}
+			return distube.play(voiceChannel, args.join(" "));
 		} else {
 			message.channel.send(
-				'You must join a voice channel first.',
+				'>>> សូមចូល Voice Channel ជាមុនសិនមុននឹងវាយ cmd!😆',
+			);
+			return;
+		}
+	}
+
+	if (cmd === "repeat" || cmd === "rp" || cmd === "loop") {
+		if(args[0].toString() === "on"){
+			distube.setRepeatMode(message, 1)
+			message.reply("រង្វិលជុំត្រូវបានបើកនៅលើបទនេះ! 🔂");
+		}else if (args[0].toString() === "off"){
+			distube.setRepeatMode(message, 0)
+			message.reply("រង្វិលជុំត្រូវបានបិទ! 📴");
+		}else if (args[0].toString() === "queue" || args[0].toString() === "q"){
+			distube.setRepeatMode(message, 2)
+			message.reply("រង្វិលជុំត្រូវបានបើកនៅលើបញ្ជីទាំងមូល! 🔁");
+		}
+	}
+
+	if (cmd === 'stop' || cmd === 's') {
+		distube.stop(message);
+		message.channel.send('ចម្រៀងត្រូវបានបញ្ឈប់! 😵‍💫');
+	}
+
+	if (cmd === 'leave' || cmd === 'l') {
+		distube.voices.get(message)?.leave();
+		message.channel.send('បានចាកចេញ, ជួប​គ្នា​ពេល​ក្រោយ! 😘');
+	}
+
+	if (cmd === 'resume' || cmd === 'rs') {
+		distube.resume(message);
+		message.reply("ចម្រៀងបានចាក់បន្ត! ▶️");
+	}
+
+	if (cmd === 'pause' || cmd === 'ps') {
+		distube.pause(message);
+		message.reply("ចម្រៀងបានស្កុប! ⏹️");
+	}
+
+	if (cmd === 'skip' || cmd === 'n' || cmd === 'next') {
+		const queue = distube.getQueue(message);
+		if(queue.songs.length > 1){
+			distube.skip(message);
+		}else{
+			message.reply('អត់មានចម្រៀងក្នុងបញ្ជី! 😕');
+		}
+	}
+
+	if (cmd === 'queue' || cmd === 'q') {
+		const queue = distube.getQueue(message);
+		if (!queue) {
+			message.channel.send('អត់មានអីចាក់ទេ! 😕');
+		} else {
+			message.channel.send(
+				`បទក្នុងបញ្ជី:\n${queue.songs
+					.map(
+						(song, id) =>
+							`**${id ? id : '🔊 កំពុងចាក់'}**. ${
+								song.name
+							} - \`${song.formattedDuration}\``,
+					)
+					.slice(0, 10)
+					.join('\n')}`,
 			);
 		}
 	}
 });
+
+distube
+	.on("playSong", (queue,song) => {
+		let playembed = new Discord.EmbedBuilder()
+		.setColor(Discord.Colors.Red)
+		.setTitle(`🎶 កំពុងចាក់ `)
+		.setDescription(`[${song.name}]`)
+		.setImage(song.thumbnail)
+		.setTimestamp()
+		.addFields({ name: 'រយៈពេល ៈ', value: song.formattedDuration.toString(), inline: true })
+		.setFooter({ text: `Request By 🔸 ${author.username} 🔸`, iconURL: author.displayAvatarURL({ dynamic: true })});
+		channel.send({ embeds: [playembed] });
+	}).on("addSong", (queue, song) => {
+		let playembed = new Discord.EmbedBuilder()
+		.setColor(Discord.Colors.Green)
+		.setTitle(`📢 បានដាក់ទៅក្នុងបញ្ជីចម្រៀង `)
+		.setDescription(`[${song.name}]`)
+		.setImage(song.thumbnail)
+		.setTimestamp()
+		.addFields({ name: 'រយៈពេល ៈ', value: song.formattedDuration.toString()})
+		.setFooter({ text: `Request By 🔹 ${author.username} 🔹`, iconURL: author.displayAvatarURL({ dynamic: true })});
+		channel.send({ embeds: [playembed] });
+	}).on('finish', () => {
+		channel.send('អស់ចម្រៀងនៅក្នុងបញ្ចី! 👏🏻');
+	})
 
 client.login(process.env.TOKEN);
